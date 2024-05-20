886be11c2e0ea5ff21d335a0d8fde5e5ab48a21d
iliavardanidze <ivardanidze@greenschool.edu.ge>
2024-05-20 09:53:39 -0700

    commit

---
+++ level 01/homework/homework.py
@@ -0,0 +1,13 @@
+print("I love Messi. Messi grew up in such a family where the father was a loth and there were many in the family and the mother alone could not do anything. The mother alone bought Messi's shoes, she went to two workshops and became like this.")
+
+
+name=3
+name1=4.2
+print(name+name1)
+
+
+
+
+print('A string is a given symbol in parentheses.')
+print('A float is any decimal number.')
+print("An integer is an integer number, eg 1, 2, 3, 4")
